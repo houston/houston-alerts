147b0327434aac11ad47f1064fbe200b09d6508d
Bob Lail <bob.lail@cph.org>
2014-09-02 10:24:03 -0500

    [fix] Tried posting time for Alerts with GET rather than POST (2m)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Houston::Alerts::Engine.routes.draw do
   get "", to: "alerts#index", as: :alerts
   get "dashboard", to: "alerts#dashboard"
   
-  post "timekeeping", to: "alerts#time", as: :timekeeping
+  get "timekeeping", to: "alerts#time", as: :timekeeping
   get "thanks", to: "alerts#thanks"
   
 end

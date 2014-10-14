49e63c018506b35bb7eadd077c4da160d37687ee
Bob Lail <bob.lail@cph.org>
2014-10-13 19:33:14 -0500

    [refactor] Moved assignment of alerts' default workers to config (5m)

--- lib/houston/alerts/configuration.rb
+++ lib/houston/alerts/configuration.rb
@@ -3,7 +3,6 @@ module Houston::Alerts
     
     def initialize
       @workers_proc = Proc.new { User.developers.unretired }
-      @default_worker_for_proc = Proc.new { |alert| nil }
       config = Houston.config.module(:alerts).config
       instance_eval(&config) if config
     end
@@ -13,14 +12,6 @@ module Houston::Alerts
         @workers_proc = block
       else
         @workers_proc.call(*args)
-      end
-    end
-    
-    def default_worker_for(*args, &block)
-      if block
-        @default_worker_for_proc = block
-      else
-        @default_worker_for_proc.call(*args)
       end
     end
     

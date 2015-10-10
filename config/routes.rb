04d2b6cc8ed5f51a1f742ce3cbc5ff62d1783838
Bob Lail <bob.lail@cph.org>
2015-10-10 11:22:49 -0500

    [skip] Namespaced routes (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Houston::Alerts::Engine.routes.draw do
 
-  get "", to: "alerts#index", as: :alerts
-  get "excel", to: "alerts#excel", as: :alerts_excel
-  get "dashboard", to: "alerts#dashboard"
+  scope "alerts" do
+    get "", to: "alerts#index", as: :alerts
+    get "excel", to: "alerts#excel", as: :alerts_excel
+    get "dashboard", to: "alerts#dashboard"
 
-  match ":id", to: "alerts#update", via: [:put, :patch]
+    match ":id", to: "alerts#update", via: [:put, :patch]
 
-  namespace "api" do
-    namespace "v1" do
-      get "alerts", to: "alerts#index"
-      get "alerts/mine", to: "alerts#mine"
+    namespace "api" do
+      namespace "v1" do
+        get "alerts", to: "alerts#index"
+        get "alerts/mine", to: "alerts#mine"
+      end
     end
-  end
 
-  get ":type/:number", to: "alerts#show", as: :alert
+    get ":type/:number", to: "alerts#show", as: :alert
+  end
 
 end

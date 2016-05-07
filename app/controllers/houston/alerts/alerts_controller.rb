3eaf120786308ce94f09e9a0a82cc1d86a94d71d
Bob Lail <bob.lailfamily@gmail.com>
2016-05-07 18:16:26 -0500

    [skip] Removed the graph of incoming Alerts from the dashboard (2m)
    This reverts commit 5f1991af53a752c5b859f2b87d3079dbc2d14169.

--- app/controllers/houston/alerts/alerts_controller.rb
+++ app/controllers/houston/alerts/alerts_controller.rb
@@ -23,34 +23,8 @@ module Houston
         @title = "Alerts (#{@count})"
         @alerts = @alerts.limit(@limit) if @limit > 0
 
-        @alerts_range = 15.weeks.ago...Time.now
-        @alerts_labels = %w{late on-time active}
-        @alerts_colors = [
-          "rgba(233, 95, 68, 1)",         # late (foreground color when .dashboard.red)
-          "rgba(255, 255, 230, 0.435)",   # on-time
-          "rgba(255, 255, 230, 0.435)"    # not due yet
-        ]
-
-        time_series = <<-SQL
-          INNER JOIN generate_series('#{@alerts_range.begin}'::date, '#{@alerts_range.end}'::date, '1 day') AS days(day)
-          ON alerts.opened_at::date = days.day
-        SQL
-        status = <<-SQL
-          (CASE WHEN alerts.deadline > now() THEN 'active'
-                WHEN alerts.closed_at < alerts.deadline THEN 'on-time'
-                ELSE 'late' END)
-        SQL
-        counter = Hash.new { |hash, key| hash[key] = Hash.new(0) }
-        @alerts_data = Houston::Alerts::Alert.all
-          .joins(time_series)
-          .group("days.day", '"status"')
-          .reorder("days.day", '"status"')
-          .pluck("days.day", "#{status} \"status\"", "COUNT(*)")
-          .each_with_object(counter) { |(date, status, count), hash| hash[date][status] = count }
-          .map { |date, counts| [date, *counts.values_at(*@alerts_labels)] }
-
         respond_to do |format|
-          format.json { render json: {alerts: AlertPresenter.new(@alerts), count: @count, graph: @alerts_data} }
+          format.json { render json: {alerts: AlertPresenter.new(@alerts), count: @count} }
           format.html { render layout: "houston/alerts/dashboard" }
         end
       end
